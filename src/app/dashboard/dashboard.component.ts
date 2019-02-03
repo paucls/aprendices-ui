3776884d4a36438ef1d780840f0d244aa457de47
Paulo Esteban <Paulo.Esteban@emc.com>
2019-02-03 16:53:35 +0000

    Display avatar logo and date on the post card

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,22 +10,26 @@ export class DashboardComponent {
     {
       author: 'John Doe',
       content: 'Accelerate\'s definition of developer productivity, Will Larson (@Lethai) \n' +
-        'https://lethain.com/accelerate-developer-productivity/'
+        'https://lethain.com/accelerate-developer-productivity/',
+      date: new Date('2018-09-10T00:00:00')
     },
     {
       author: 'John Doe',
       content: 'The Life-Changing Magic of Tidying Up Code, Kent Beck (@KentBeck) \n' +
-        'https://www.facebook.com/notes/kent-beck/the-life-changing-magic-of-tidying-up-code/1544047022294823/'
+        'https://www.facebook.com/notes/kent-beck/the-life-changing-magic-of-tidying-up-code/1544047022294823/',
+      date: new Date('2018-09-10T00:00:00')
     },
     {
-      author: 'John Doe',
+      author: 'Clark Kent',
       content: 'How long is too long for a build?, Brett L. Schuchert (@schuchert) \n' +
-        'https://www.industriallogic.com/blog/how-long-is-too-long-for-a-build/'
+        'https://www.industriallogic.com/blog/how-long-is-too-long-for-a-build/',
+      date: new Date('2018-09-13T00:00:00')
     },
     {
       author: 'John Doe',
       content: 'The Right Time for Testing, Maaret (@maaretp) \n' +
-        'http://visible-quality.blogspot.com/2018/09/the-right-time-for-testing.html'
+        'http://visible-quality.blogspot.com/2018/09/the-right-time-for-testing.html',
+      date: new Date('2018-09-15T00:00:00')
     }
   ];
 
