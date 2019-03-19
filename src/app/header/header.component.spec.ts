cb323a5b5485945f2cdb01fff282755d5a0a5f92
Paulo Clavijo Esteban <paucls@gmail.com>
2019-03-19 20:40:28 +0000

    Adding about button on the header

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { MatToolbarModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -10,7 +11,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatToolbarModule],
+      imports: [MatToolbarModule, RouterTestingModule],
       declarations: [HeaderComponent]
     }).compileComponents();
   }));
